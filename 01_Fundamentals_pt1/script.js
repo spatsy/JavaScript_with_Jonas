77f865d55c4026b39eea39586ca7fa05174833d9
Steve Patsy <122055180+spatsy@users.noreply.github.com>
2023-11-28 11:18:43 -0800

    Add files via upload

---
+++ 01_Fundamentals_pt1/script.js
@@ -0,0 +1,5 @@
+//we just added the script tag in this demo I also played aroung with the body tag and changed the color and gradient.if we placed this in the head tag, this would be an inline script but it's generally frowned upon
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!')
+//this is a console log and only shows up in the browser console and doesn't get printed on the page -->
+console.log(40 + 8 + 23 - 11);
